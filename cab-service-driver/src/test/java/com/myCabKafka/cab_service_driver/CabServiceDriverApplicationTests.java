93a05706f2750baee8cf26e9784586b565cac365
avimehta14 <avimehta14@gmail.com>
2024-08-11 00:47:42 -0500

    kafka project setup

---
+++ cab-service-driver/src/test/java/com/myCabKafka/cab_service_driver/CabServiceDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myCabKafka.cab_service_driver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabServiceDriverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ cab-service-user/src/test/java/com/myCabKafka/cab_service_user/CabServiceUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myCabKafka.cab_service_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabServiceUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
