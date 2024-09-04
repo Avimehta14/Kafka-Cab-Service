0a0d1fff67fd927c5310c936c846b2f59b33533f
avimehta14 <avimehta14@gmail.com>
2024-09-04 14:01:20 -0500

    final topic

--- cab-service-user/src/main/java/com/myCabKafka/cab_service_user/service/FinalLocationConsumer.java
+++ cab-service-user/src/main/java/com/myCabKafka/cab_service_user/service/FinalLocationConsumer.java
@@ -1,4 +1,17 @@
 package com.myCabKafka.cab_service_user.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+
 public class FinalLocationConsumer {
+
+    private static final Logger logger = LoggerFactory.getLogger(FinalLocationConsumer.class);
+
+    @KafkaListener(topics="final-topic",groupId = "user-group")
+    public void finalListenerTopic(String location)
+    {
+        System.out.println("Final location received in final topic: "+location);
+        logger.info("Final location received in final topic:" +location);
+    }
 }
